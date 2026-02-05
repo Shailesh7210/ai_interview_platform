dcfff2e024ae848c2dd35aff474c6bcafbe23454
Shailesh7210 <shailjaiswal9135@gmail.com>
2026-02-05 17:20:09 +0530

    stream authentication added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,27 @@
 import express from "express";
-import {ENV} from './lib/env.js'
 import path from 'path'
+import cors from "cors"
+import {serve} from "inngest/express"
+
 import { connectDB } from "./lib/db.js";
+import {ENV} from './lib/env.js'
+import { inngest, functions } from "./lib/inngest.js";
+
 
 
 console.log(ENV.PORT)
 const app = express();
 
 const __dirname = path.resolve()
+
+
+//Middlewares
+app.use(express.json())
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
+
+app.use("/api/inngest", serve({client: inngest, functions}))
+
 
 app.use('/health',(req,res)=>{
     res.status(200).json({message: "this is health endpoint"})
