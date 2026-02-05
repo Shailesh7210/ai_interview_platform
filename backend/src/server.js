f38219f70c39eb442be6e8c1de270e7c307134c5
Shailesh7210 <shailjaiswal9135@gmail.com>
2026-02-05 21:41:51 +0530

    Error solved

--- backend/src/server.js
+++ backend/src/server.js
@@ -38,7 +38,7 @@ app.use('/book',(req,res)=>{
 if(ENV.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
     app.get("/{*any}",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"))
+        res.sendFile(path.join(__dirname,"../frontend/dist", "index.html"))
     })
 }
 
